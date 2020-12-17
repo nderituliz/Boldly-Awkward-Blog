57ba3d19c24dda089e8b0a7c7abd043545a8554e
Elizabeth Nderitu <nderituliz@gmail.com>
2020-12-17 17:59:07 +0300

    created a function for the blog form

--- app/admin/views.py
+++ app/admin/views.py
@@ -15,4 +15,25 @@ def check_user():
         abort (403)
 
     return render_template('admin/dashboard.html')
+
+@admin.route('/blog/new', methods=['GET', 'POST'])
+@login_required
+def new_blog():
+
+    form = BlogForm()
+
+    if form.validate_on_submit():
+
+        title = form.title.data
+        description = form.description.data
+        blog = form.blog.data
+
+        new_blog = Blog(blog=blog, title=title, description=description)
+
+        new_blog.save_blog()
+
+        return redirect(url_for('main.index'))
+
+    return render_template('new_blog.html', blog_form=form)
+
     
