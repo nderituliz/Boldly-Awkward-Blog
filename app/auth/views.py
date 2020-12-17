1ad4d69b09b6cee503dff5983c9e155513fa7ec4
Elizabeth Nderitu <nderituliz@gmail.com>
2020-12-17 20:47:30 +0300

    Creatd a register function to register our users

--- app/auth/views.py
+++ app/auth/views.py
@@ -24,3 +24,22 @@ def login():
     title = 'Login'
 
     return render_template('auth/login.html', login_form=login_form, title=title)
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('auth.login'))
+
+    title = 'Create Account'
+
+    return render_template('auth/register.html', registration_form=form, title=title)
