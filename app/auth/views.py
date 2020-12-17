2990ccfcbd81f132a3fca2f8558b69047aa31714
Elizabeth Nderitu <nderituliz@gmail.com>
2020-12-17 20:48:43 +0300

    Created a logout function to logout our users

--- app/auth/views.py
+++ app/auth/views.py
@@ -43,3 +43,12 @@ def register():
     title = 'Create Account'
 
     return render_template('auth/register.html', registration_form=form, title=title)
+
+@auth.route('/logout')
+@login_required
+
+def logout():
+
+    logout_user()
+
+    return redirect(url_for('main.index'))
