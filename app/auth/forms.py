29b615fbaab6bbc60db02726b43bb6ef009e9462
Elizabeth Nderitu <nderituliz@gmail.com>
2020-12-17 20:37:51 +0300

    Created a registration form with the help of the FlaskForm Class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,3 +3,22 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, Valid
 from wtforms.validators import Required, Email, EqualTo
 
 from ..models import User
+
+
+#registration form for new users
+class RegistrationForm(FlaskForm):
+
+    email = StringField('Enter Your Email Address', validators=[Required(), Email()])
+    username = StringField('Enter User Name', validators=[Required()])
+    password = PasswordField('Enter Password', validators=[Required(),
+                                                     EqualTo('password_confirm', message='Passwords do not match!')])
+    password_confirm = PasswordField('Confirm Passwords', validators=[Required()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self, data_field):
+        if User.query.filter_by(email=data_field.data).first():
+            raise ValidationError('Email already exists')
+
+    def validate_username(self, data_field):
+        if User.query.filter_by(username=data_field.data).first():
+            raise ValidationError('Username already taken')
