47c29921c3e4bacc2b73fa50e6b1ce290e6b952b
Elizabeth Nderitu <nderituliz@gmail.com>
2020-12-17 17:34:08 +0300

    created the class for te blog form

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -2,3 +2,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import Required
+
+class BlogForm(FlaskForm):
+
+    title = StringField('Blog Title', validators=[Required()])
+
+    description = StringField('Description', validators=[Required()])
+
+    blog = TextAreaField('Blog')
+
+    submit = SubmitField('Submit')
