36812782a6da6154ba8726a78be292f9c9810896
Elizabeth Nderitu <nderituliz@gmail.com>
2020-12-17 20:58:47 +0300

    Added a class to create our blog forms

---
+++ app/main/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import Required
+
+
+class BlogForm(FlaskForm):
+
+    title = StringField('Blog Title', validators=[Required()])
+
+    description = StringField('Blog Description', validators=[Required()])
+
+    blog = TextAreaField('Blog')
+
+    submit = SubmitField('Submit')
