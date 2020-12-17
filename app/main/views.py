53920bf0b5563edf572097ee76ec6764ee9a3628
Elizabeth Nderitu <nderituliz@gmail.com>
2020-12-17 21:13:09 +0300

    Added a route function to create a new review

--- app/main/views.py
+++ app/main/views.py
@@ -51,3 +51,27 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
+
+
+@main.route('/blog/review/new/<int:id>', methods=['GET', 'POST'])
+@login_required
+def new_review(id):
+
+    blog = Blog.query.filter_by(id=id).first()
+
+    if blog is None:
+        abort(404)
+
+    form = ReviewForm()
+
+    if form.validate_on_submit():
+
+        review = form.review.data
+
+        new_review = Review(review=review, user_id=current_user.id, blog_id=blog.id)
+
+        new_review.save_review()
+
+        return redirect(url_for('.single_blog', id=blog.id))
+
+    return render_template('new_review.html', review_form=form, blog=blog)
