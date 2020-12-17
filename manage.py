550449be90b46b28c52cf7d4ed9b0043c0a68e3c
Elizabeth Nderitu <nderituliz@gmail.com>
2020-12-17 23:08:29 +0300

    Push to heroku

---
+++ manage.py
@@ -0,0 +1,32 @@
+from flask_script import Manager,Server
+from app import create_app,db
+from app.models import User, Blog, Review, Role,Quote,Subscribers
+from flask_migrate import Migrate, MigrateCommand
+
+#Creating app instance
+app = create_app('production')
+# app = create_app('production')
+
+#Manager Commands
+manager = Manager(app)
+migrate = Migrate(app,db)
+
+#Manager Functionalities
+manager.add_command('server', Server)
+manager.add_command('db',MigrateCommand)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, Blog = Blog, User = User, Review = Review, Role = Role, Subscribers = Subscribers, Quote = Quote)
+
+#Testing Settings
+@manager.command
+def test():
+    """Run unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__ == '__main__':
+    manager.run()
---
+++ start.sh
@@ -0,0 +1,3 @@
+export MAIL_USERNAME='nderituliz@gmail.com'
+export MAIL_PASSWORD='password2020'
+python3.8 manage.py server
